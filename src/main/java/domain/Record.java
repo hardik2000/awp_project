277e01a9a4ca7c86943408efa9188eee47731777
Kaustubh Kathare <kaustubhkkathare@gmail.com>
2020-05-23 10:03:27 +0530

    added in the domain package

--- src/main/java/domain/Record.java
+++ src/main/java/domain/Record.java
@@ -9,6 +9,51 @@ package domain;
  *
  * @author HARDIK
  */
-public class Record {
+public class Record{
+    private String username_booked;
+    private String number_adults;
+    private String number_children;
+    private String date_booking;
+    private String date_checkout;
+
+    public String getUsername_booked() {
+        return username_booked;
+    }
+
+    public void setUsername_booked(String username_booked) {
+        this.username_booked = username_booked;
+    }
+
+    public String getNumber_adults() {
+        return number_adults;
+    }
+
+    public void setNumber_adults(String number_adults) {
+        this.number_adults = number_adults;
+    }
+
+    public String getNumber_children() {
+        return number_children;
+    }
+
+    public void setNumber_children(String number_children) {
+        this.number_children = number_children;
+    }
+
+    public String getDate_booking() {
+        return date_booking;
+    }
+
+    public void setDate_booking(String date_booking) {
+        this.date_booking = date_booking;
+    }
+
+    public String getDate_checkout() {
+        return date_checkout;
+    }
+
+    public void setDate_checkout(String date_checkout) {
+        this.date_checkout = date_checkout;
+    }
     
 }
--- src/main/java/domain/Room.java
+++ src/main/java/domain/Room.java
@@ -9,6 +9,51 @@ package domain;
  *
  * @author HARDIK
  */
-public class Room {
+public class Room{
+    private String type;
+    private String id;
+    private String price;
+    private String capacity;
+    private String available_rooms;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getAvailable_rooms() {
+        return available_rooms;
+    }
+
+    public void setAvailable_rooms(String available_rooms) {
+        this.available_rooms = available_rooms;
+    }
     
 }
